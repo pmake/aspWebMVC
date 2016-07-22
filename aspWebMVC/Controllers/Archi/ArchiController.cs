5ad1ca241e63f1a24a39e0c2a014fa730f6c8486
Archi <unique.ntsn+git@gmail.com>
2016-07-22 16:04:28 +0800

    initial commit 進行了基礎操作 http://longtermpower.blogspot.tw/2016/07/asp-web-mvc-memo.html

---
+++ aspWebMVC/Controllers/Archi/ArchiController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace aspWebMVC.Controllers
+{
+    public class ArchiController : Controller
+    {
+        // GET: Archi
+        public ActionResult Index()
+        {
+            UserViewModel uvm = new UserViewModel();
+            uvm.name = "Archi";
+            uvm.address = "中壢市區";
+            uvm.isShow = true;
+            return View(uvm);
+        }
+        public ActionResult Products()
+        {
+            UserViewModel uvm1 = new UserViewModel();
+            uvm1.name = "Archi";
+            UserViewModel uvm2 = new UserViewModel();
+            uvm2.name = "Wen";
+            List<UserViewModel> list = new List<UserViewModel>();
+            list.Add(uvm1);
+            list.Add(uvm2);
+
+            return View(list);
+        }
+    }
+    public class UserViewModel
+    {
+        public string name { set; get; }
+        public string phone { set; get; }
+        public string address { set; get; }
+        public bool isShow { set; get; }
+    }
+}
